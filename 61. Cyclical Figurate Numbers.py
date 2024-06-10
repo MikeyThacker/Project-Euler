c8c4ddd102cff646b9d1113d51ac74ca45fa7634
mikey <mikeythacker@icloud.com>
2024-06-11 00:22:36 +0100

    Working on #61. Only need to work through connections to find route

--- 61. Cyclical Figurate Numbers.py
+++ 61. Cyclical Figurate Numbers.py
@@ -1,99 +1,110 @@
 from math import *
 
 
-def get_triangle_numbers():
-    triangle_numbers = []
+def get_nums():
+    nums = []
+
+    # Triangle Numbers
     n = 1
     while True:
-        next_num = (n * (n + 1) // 2)
-        if next_num >= 10_000:
-            return triangle_numbers
-        if next_num > 999:
-            triangle_numbers.append(next_num)
+        next = n * (n + 1) // 2
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 3))
         n += 1
 
-
-def get_square_numbers():
-    numbers = [x for x in range(1000, 10_000) if sqrt(x) % 1 == 0]
-    return numbers
-
-
-def get_pentagonal_numbers():
-    pentagonal_numbers = []
+    # Square Numbers
     n = 1
     while True:
-        next_num = (n * ((3 * n) - 1) // 2)
-        if next_num >= 10_000:
-            return pentagonal_numbers
-        if next_num > 999:
-            pentagonal_numbers.append(next_num)
+        next = n ** 2
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 4))
         n += 1
 
-
-def get_hexagonal_numbers():
-    hexagonal_numbers = []
+    # Pentagon Numbers
     n = 1
     while True:
-        next_num = n * ((2 * n) - 1)
-        if next_num >= 10_000:
-            return hexagonal_numbers
-        if next_num > 999:
-            hexagonal_numbers.append(next_num)
+        next = n * ((3 * n) - 1) // 2
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 5))
         n += 1
 
-
-def get_heptagonal_numbers():
-    heptagonal_numbers = []
+    # Hexagon Numbers
     n = 1
     while True:
-        next_num = (n * ((5 * n) - 3) // 2)
-        if next_num >= 10_000:
-            return heptagonal_numbers
-        if next_num > 999:
-            heptagonal_numbers.append(next_num)
+        next = n * ((2 * n) - 1)
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 6))
         n += 1
 
-
-def get_octagonal_numbers():
-    octagonal_numbers = []
+    # Heptagon Numbers
     n = 1
     while True:
-        next_num = n * ((3 * n) - 2)
-        if next_num >= 10_000:
-            return octagonal_numbers
-        if next_num > 999:
-            octagonal_numbers.append(next_num)
+        next = n * ((5 * n) - 3) // 2
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 7))
         n += 1
 
+    # Octagon Numbers
+    n = 1
+    while True:
+        next = n * ((3 * n) - 2)
+        if next > 10_000:
+            break
+        if next >= 1_000:
+            nums.append((next, 8))
+        n += 1
 
-def has_property(num1, num2):
+    return nums
+
+
+def check_property(num1, num2):
     """
-    :param num1: Num to check first two digits of
-    :param num2: Num to check last two digits of
-    :return: Boolean
+    :param num1: Number to check last two digits of
+    :param num2: Number to check first two digits of
+    :return: boolean
     """
-
-    first = str(num1)[:2]
-    last = str(num2)[2:]
+    last = str(num1)[2:]
+    first = str(num2)[:2]
     return first == last
 
 
-def check_lists(list1, list2):
-    """
-    :param list1: List to check first two digits of each number
-    :param list2: List to check last two digits of each number
-    :return:
-    """
+def get_connections(all_nums):
+    dictionary = {}
+    for num1 in all_nums:
+        cyclic_to_x = []
+        for num2 in all_nums:
+            if num1[0] == num2[0]:  # If numbers are same value
+                continue
+            elif num1[1] == num2[1]:  # If numbers are same shape
+                continue
+            elif not check_property(num1[0], num2[0]):
+                continue
+            cyclic_to_x.append(num2)
+        dictionary[num1] = cyclic_to_x
+    return dictionary
+
+
+def find_route(connections):
+    # TODO Step 3: https://www.ivl-projecteuler.com/overview-of-problems/20-difficulty/problem-61
+    return None
 
 
 def main():
-    triangle_numbers = get_triangle_numbers()
-    square_numbers = get_square_numbers()
-    pentagonal_numbers = get_pentagonal_numbers()
-    hexagonal_numbers = get_hexagonal_numbers()
-    heptagonal_numbers = get_heptagonal_numbers()
-    octagonal_numbers = get_octagonal_numbers()
-    pass
+    all_nums = get_nums()
+    connections = get_connections(all_nums)
+    solution = find_route(connections)
 
 
 print(main())
+
+# [8256, 5625, 2512, 1281, 8128, 2882]
