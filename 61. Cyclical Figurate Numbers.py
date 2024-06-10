c5b5cf46656c2fd1cd59eb22ecdfda82d1b8e0bc
mikey <mikeythacker@icloud.com>
2024-06-10 22:22:40 +0100

    Restart #61

--- 61. Cyclical Figurate Numbers.py
+++ 61. Cyclical Figurate Numbers.py
@@ -66,42 +66,24 @@ def get_octagonal_numbers():
         n += 1
 
 
-def list_of_tuples_to_list(old_list):
-    new_list = []
-    for tuple in old_list:
-        new_list.append(tuple[1])
-    return sorted(list(set(new_list)))
+def has_property(num1, num2):
+    """
+    :param num1: Num to check first two digits of
+    :param num2: Num to check last two digits of
+    :return: Boolean
+    """
 
-
-def find_previous_link(num_search, list):
-    for tuple in list:
-        if tuple[1] == num_search:
-            return tuple[0]
+    first = str(num1)[:2]
+    last = str(num2)[2:]
+    return first == last
 
 
 def check_lists(list1, list2):
-    '''
-    :param list1: list checking last two numbers match
-    :param list2: list checking first two numbers match
-    :return: dict containing first number leading to second
-    '''
-    list_of_works = []
-    for num1 in list1:
-        last_two = str(num1)[2:]
-
-        for num2 in list2:
-            first_two = str(num2)[:2]
-
-            if last_two == first_two:
-                list_of_works.append((num1, num2))
-
-    return list_of_works
-
-
-def find_solution(num_to_find, list):
-    for tuple in list:
-        if tuple[1] == num_to_find:
-            return tuple[0]
+    """
+    :param list1: List to check first two digits of each number
+    :param list2: List to check last two digits of each number
+    :return:
+    """
 
 
 def main():
@@ -111,33 +93,7 @@ def main():
     hexagonal_numbers = get_hexagonal_numbers()
     heptagonal_numbers = get_heptagonal_numbers()
     octagonal_numbers = get_octagonal_numbers()
-
-    # Find chain
-    works3_4 = check_lists(triangle_numbers, square_numbers)
-    works4_5 = check_lists(list_of_tuples_to_list(works3_4), pentagonal_numbers)
-    works5_6 = check_lists(list_of_tuples_to_list(works4_5), hexagonal_numbers)
-    works6_7 = check_lists(list_of_tuples_to_list(works5_6), heptagonal_numbers)
-    works7_8 = check_lists(list_of_tuples_to_list(works6_7), octagonal_numbers)
-
-    # Check numbers loop
-    works8_3 = 0
-
-    # Find Chain
-    num8 = works8_3[0][0]
-    num7 = find_previous_link(num8, works7_8)
-    num6 = find_previous_link(num7, works6_7)
-    num5 = find_previous_link(num6, works5_6)
-    num4 = find_previous_link(num5, works4_5)
-    num3 = find_previous_link(num4, works3_4)
-
-    nums = [num3, num4, num5, num6, num7, num8]
-    print(nums)
-    return sum(nums)
-
-
-
+    pass
 
 
 print(main())
-
-# [8256, 5625, 2512, 1281, 8128, 2882]
