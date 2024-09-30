986f422afe64b0182ab2f330cd270f473cd66909
mikeythacker <mikeythacker@icloud.com>
2024-09-30 12:16:07 +0100

    Started #70.

---
+++ 70. Totient Permutation.py
@@ -0,0 +1,14 @@
+from math import *
+
+def phi(n):
+    count = 1
+    for i in range(2, n):
+        if lcm(n, i) == n * i:
+            count += 1
+    return count
+
+def main():
+    for n in range(1, 10 ** 7):
+        print(f"{n}: {phi(n)}")
+
+print(main())
