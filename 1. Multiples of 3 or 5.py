4989ee68ea1cb89adbeb42350c0c2f8335df9e55
mikeythacker <mikeythacker@icloud.com>
2024-10-01 15:06:33 +0100

    Cleanup #1

--- 1. Multiples of 3 or 5.py
+++ 1. Multiples of 3 or 5.py
@@ -6,12 +6,15 @@ def main():
     sum = sum_of_list(multiples)
     print(sum)
 
+
 def is_multiple_of_3_or_5(n):
     if n % 3 == 0 or n % 5 == 0:
         return True
     return False
 
+
 def sum_of_list(list):
     return sum(list)
+
 
 main()
