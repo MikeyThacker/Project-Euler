0b65c1001aa4f51dcc20569166fb76b2c6348e99
mikeythacker <mikeythacker@icloud.com>
2024-10-04 15:05:33 +0100

    #72 - find number of midpoint fractions

--- 72. Counting Fractions.py
+++ 72. Counting Fractions.py
@@ -1,4 +1,32 @@
+from fractions import *
+
+
+def middle_fraction(frac1, frac2):
+    a = frac1.numerator
+    b = frac1.denominator
+    c = frac2.numerator
+    d = frac2.denominator
+    new_fraction = Fraction(a + c, b + d)
+
+    return new_fraction
+
+
+def find_fractions_between(frac1, frac2, count):
+    mid = middle_fraction(frac1, frac2)
+    if mid.denominator > 1_000_000:
+        return count
+
+    count += find_fractions_between(frac1, mid.numerator, count)
+    return count
+
+
 def main():
-    return 0
+    upper = Fraction(1, 1)
+    lower = Fraction(0, 1)
+
+    count = find_fractions_between(upper, lower, 0)
+
+    return count
+
 
 print(main())
