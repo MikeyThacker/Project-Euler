adda3310548e2c497f4ea96505aab25b67eee361
mikeythacker <mikeythacker@icloud.com>
2024-10-12 01:12:22 +0100

    #72, So close with this. Changed factorisation so it is only done once, and how to calculate number of fractions to a sum that works for all numerators

--- 72. Counting Fractions.py
+++ 72. Counting Fractions.py
@@ -1,6 +1,5 @@
-from decimal import *
-from math import *
 from itertools import count
+from math import *
 
 
 def get_primes(MAX):
@@ -19,12 +18,49 @@ def get_primes(MAX):
     return primes
 
 
-def get_prime_factors(n, primes):
-    factors = []
-    for prime in primes:
-        if n % prime == 0:
-            factors.append(prime)
-    return factors
+def get_prime_factors(n, primes, known_factors):
+
+    original_n = n
+    factors = set([])
+    prime_index = 0
+
+    while n != 1:
+        if n in known_factors:
+            for factor in known_factors[n]:
+                factors.add(factor)
+            known_factors[original_n] = factors
+            return factors, known_factors
+        current_prime = primes[prime_index]
+
+        if current_prime > n //2:
+            factors.add(n)
+            break
+        if n % current_prime == 0:
+            # If current prime is a factor of n
+
+            factors.add(current_prime)
+            n //= current_prime
+        else:
+            prime_index += 1
+
+    known_factors[original_n] = factors
+    return list(factors), known_factors
+
+
+def get_count(d, n, primes, known_factors):
+    """
+    Take number of fractions left (d-n),
+    Multiply by (x-1)/x for x in fractions
+        This removes every nth fraction
+    """
+    product = d - n
+    prime_factors, known_factors = get_prime_factors(n, primes, known_factors)
+
+    for prime in prime_factors:
+        product *= (prime - 1) / prime
+    product = ceil(product)
+
+    return product, known_factors
 
 
 def main():
@@ -32,8 +68,6 @@ def main():
     range n from 1 to d
 
     Generate count of every fraction < 1 with numerator n
-    For every even n, count = (d-n) / 2
-    For every odd n, count = (d-n) - ((d-n) DIV n), Remove every nth fraction
     (d-n) removes any fractions created where the numerator is greater than the denominator
         and the fraction is therefore greater than one
     """
@@ -41,23 +75,22 @@ def main():
     d = 1_000_000
     count = d - 1
     primes = get_primes(d)
+    # Keep dictionary of prime factors to reference later so smaller numbers not being calculated lots of times
+    known_factors = {}
 
     for n in range(2, d):
-        getcontext().prec = 100
-        if n % 2 == 0:  # If n is even:
-            this_count = Decimal((d - n) / 2)
-        else:
-            smallest_prime_factor = get_prime_factors(n, primes)
-            # TODO calculate sum based on prime factors
+        this_count, known_factors = get_count(d, n, primes, known_factors)
 
-            remaining_fractions = d - n
-            this_count = Decimal(remaining_fractions / smallest_prime_factor)
-            this_count = Decimal(remaining_fractions - this_count)
-            this_count = ceil(this_count)
-
-        print(f"{n}: {int(this_count)}")
+        print(f"{n}: {this_count}")
         count += this_count
-    return count
-
+    return int(count)
 
 print(main())
+
+
+# 366663327241
+# 325714454051
+# 354256844782
+# 377165084807
+# 377164511818
+# 303964043372
