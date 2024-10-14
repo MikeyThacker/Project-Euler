74dc444f0ac4653bd1f42d293cab6a0b040f7da4
mikeythacker <mikeythacker@icloud.com>
2024-10-14 13:39:57 +0100

    Finished #73, Could be quicker but happy with this

--- 73. Counting  Fractions in a Range.py
+++ 73. Counting  Fractions in a Range.py
@@ -1,5 +1,32 @@
+from fractions import Fraction
+from math import gcd
+
+def middle_fraction(upper, lower):
+    return (upper + lower) * Fraction(1,2)
+
 def main():
-    return None
+    d = 12_000
+    count = 0 # Number of fractions in range with denominator <= d
+
+    upper = Fraction(1, 2)
+    lower = Fraction(1, 3)
+
+    for denominator in range(d, 4, -1):
+        for numerator in range((denominator // 3) + 1, (denominator // 2) + 1):
+            if gcd(numerator, denominator) != 1:
+                # Fraction not in its simplest form, Ignore
+                continue
+
+            fraction = Fraction(numerator, denominator)
+
+            if fraction >= upper or fraction <= lower:
+                # Fraction not in range of 1/3 to 1/2
+                continue
+
+            print(fraction)
+            count += 1
+    return count
+
 
 
 print(main())
