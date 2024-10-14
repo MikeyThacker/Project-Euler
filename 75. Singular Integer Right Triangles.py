74177b0de453a7a4e9f62a4d3e260c666413e78c
mikeythacker <mikeythacker@icloud.com>
2024-10-14 18:23:03 +0100

    Start #75

--- 75. Singular Integer Right Triangles.py
+++ 75. Singular Integer Right Triangles.py
@@ -1,4 +1,19 @@
-def main():
+def get_triples():
     return None
+
+
+def main():
+    """
+    Use Euclid's formula:
+        A Pythagorean triple can be generated using integers m and n.
+        The triple comprises a, b, and c where:
+            a = m^2 - n^2
+            b = 2mn
+            c = m^2 + n^2
+        Where m and n are coprime and one is even.
+    """
+
+    return None
+
 
 print(main())
