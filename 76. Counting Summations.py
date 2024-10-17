3e7e2a4e455c35b4605e7a3a03983f6569226f2a
mikeythacker <mikeythacker@icloud.com>
2024-10-17 14:23:48 +0100

    Finished #76. 5 sides of A4 trying to figure this one out. Tricky pattern to find.

--- 76. Counting Summations.py
+++ 76. Counting Summations.py
@@ -1,24 +1,31 @@
-def get_natural_sum(n):
-    """
-    Returns sum of every natural number <= n
-
-    Sum of natural numbers < n: n(n - 1)*0.5
-    Sum of natural numbers <= n: n + n(n - 1)*0.5
-        By rearranging:
-            Sum = n(n + 1)*0.5
-    """
-    return n * (n + 1) * 0.5
-
-
-def get_count(n, previous_count):
-    return previous_count + get_natural_sum((n//2) - 1) + 1
-
-
 def main():
-    count = 6 # Count when n = 5
+    """
+    sum_sums = list where index is n,
+        value stored is list of length of sum with starting digit = (n - index)
+    e.g., sum_sums[9] = [1, 2, 3, 5, 7, 9, 5, 1], n = 10
+
+    counts = list where nth term is number of ways to express n as a sum
+    """
+
+    sum_sums = [[], [], [1], [1, 1], [1, 2, 1], [1, 2, 2, 1]]  # 0 to 5 already filled in
+    counts = [0, 0, 1, 2, 4, 6]  # //, No ways to make 0 and 1
+
     for n in range(6, 101):
-        count = get_count(n, count)
-    return count
+        this_count = 0
+
+        # Add previous counts from 1 to n/2
+        numbers_to_add = counts[1:(n // 2) + 1]
+        numbers_to_add = [number + 1 for number in numbers_to_add]
+
+        # Add counts from n//2 to n
+        for start_digit in range((n - 1) // 2, 0, -1):
+            to_add = sum_sums[n - start_digit][-start_digit:]
+            numbers_to_add.append(sum(to_add))
+
+        # Add this n and its sums to "records"
+        sum_sums.append(numbers_to_add)
+        counts.append(sum(numbers_to_add))
+    return counts[-1]
 
 
 print(main())
